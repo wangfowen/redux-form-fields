992715534a95505e85153ff018286a468f7e8e8d
Owen Wang <wangfowen@gmail.com>
2020-03-07 14:18:15 -0500

    fix radio

--- example/src/TestForm.tsx
+++ example/src/TestForm.tsx
@@ -2,7 +2,6 @@ import { CheckboxField, RadioField, TextAreaField, TextField } from './reactComp
 
 import React from 'react';
 import { reduxForm } from 'redux-form';
-import styles from './TestForm.module.css';
 
 const TestForm = (props: any) => {
   const { handleSubmit, disabled } = props;
@@ -12,9 +11,9 @@ const TestForm = (props: any) => {
       <TextField name="email" label="Email" type="email" />
       <TextField name="presetEmail" label="Email" type="email2" isRequired isDisabled />
       <TextField name="password" label="Password" type="password" />
-      <TextAreaField name="textareaz" isRequired label="Textarea" rows={5} />
-      <RadioField name="radio" value="1" label="Test Radio 1" checked fieldCustomClass={styles.radioField} />
-      <RadioField name="radio" value="2" label="Test Radio 2" fieldCustomClass={styles.radioField} />
+      <TextAreaField name="textareaz" label="Textarea" rows={5} />
+      <RadioField name="radio1" value="1" label="Test Radio 1" />
+      <RadioField name="radio1" value="2" label="Test Radio 2" />
       <CheckboxField
         name="terms"
         label={
